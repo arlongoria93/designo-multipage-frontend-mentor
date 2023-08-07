d503cb1c173f00f84dd6d080cfac95d0dcd986c4
Angelo <arlongoria93@gmail.com>
2023-08-07 15:58:28 -0500

    update page.js and navbar.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
+import Navbar from "@/components/Navbar/navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <>
+      <Navbar />
+      <main className="flex min-h-screen flex-col p-24"></main>
+    </>
   );
 }
--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <div className="w-[375px] h-[96px] bg-white"></div>;
 };
 
 export default Navbar;
