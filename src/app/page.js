6db55bb944bd5d4f7336c38f946f76f7a99009a4
Angelo <arlongoria93@gmail.com>
2023-08-07 21:10:42 -0500

    update bg-pattern-small-circle.svg, page.js and src/components/svgs/index.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,34 @@
 import Navbar from "@/components/Navbar/navbar";
-import Image from "next/image";
+import { Jost } from "next/font/google";
+import { BackgroundPatternHeroHome } from "@/components/svgs";
 
+const jost = Jost({
+  subsets: ["latin"],
+  weights: ["400", "500", "600", "700"],
+});
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <main className="flex min-h-screen flex-col p-24"></main>
+      <Navbar />{" "}
+      <main
+        className={`${jost.className} w-[375px] h-[843px] bg-[#E7816B] flex flex-col z-10`}
+      >
+        <BackgroundPatternHeroHome className="self-center mt-[102px] h-[640px] w-[640px] ml-[265px] absolute" />
+        <div className="flex flex-col gap-[14px] mt-[80px] w-[327px] self-center">
+          <h1 className="text-white text-center font-medium text-[32px] leading-[36px]">
+            Award-winning custom designs and digital branding solutions
+          </h1>
+          <p className="text-center text-[15px] leading-[25px] text-white">
+            With over 10 years in the industry, we are experienced in creating
+            fully responsive websites, app design, and engaging brand
+            experiences. Find out more about our services.
+          </p>
+          <button className="mt-[24px] font-medium text-[15px] bg-white w-[152px] h-[56px] rounded-[8px] tracking-[1px] self-center">
+            {" "}
+            LEARN MORE
+          </button>
+        </div>
+      </main>
     </>
   );
 }
--- src/components/svgs/index.js
+++ src/components/svgs/index.js
@@ -1,1 +1,3 @@
 export { default as IconHamburger } from "../../../public/assets/shared/mobile/icon-hamburger.svg";
+export { default as BackgroundPatternSmallCircle } from "../../../public/assets/shared/desktop/bg-pattern-small-circle.svg";
+export { default as BackgroundPatternHeroHome } from "../../../public/assets/home/desktop/bg-pattern-hero-home.svg";
