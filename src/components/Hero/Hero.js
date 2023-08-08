a9f6eab2b3721f9b9311e233c258141e63fea6f3
Angelo <arlongoria93@gmail.com>
2023-08-08 10:38:50 -0500

    moved hero to its own component

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Image from "next/image";
+import { Jost } from "next/font/google";
+import { BackgroundPatternHeroHome } from "@/components/svgs";
+
+const jost = Jost({
+  subsets: ["latin"],
+  weights: ["400", "500", "600", "700"],
+});
+const Hero = () => {
+  return (
+    <main
+      className={`${jost.className} w-[375px] h-[843px] bg-[#E7816B] flex flex-col z-10 overflow-clip`}
+    >
+      <div className="relative">
+        <BackgroundPatternHeroHome className="self-center mt-[102px] h-[640px] w-[640px] absolute overflow-hidden" />
+      </div>
+      <div className="flex flex-col gap-[14px] mt-[80px] w-[327px] self-center z-20">
+        <h1 className="text-white text-center font-medium text-[32px] leading-[36px]">
+          Award-winning custom designs and digital branding solutions
+        </h1>
+        <p className="text-center text-[15px] leading-[25px] text-white">
+          With over 10 years in the industry, we are experienced in creating
+          fully responsive websites, app design, and engaging brand experiences.
+          Find out more about our services.
+        </p>
+        <button className="mt-[24px] font-medium text-[15px] bg-white w-[152px] h-[56px] rounded-[8px] tracking-[1px] self-center">
+          LEARN MORE
+        </button>
+      </div>
+      <div className="relative w-[625px] h-[700px] self-center -mt-[90px] ">
+        <Image
+          quality={100}
+          fill
+          objectFit="cover"
+          src="/assets/home/desktop/image-hero-phone.png"
+          className=" asbolute object-top"
+        />
+      </div>
+    </main>
+  );
+};
+
+export default Hero;
