ed495e13c91446ed593b2cd6d6df1318928d04f6
Angelo <arlongoria93@gmail.com>
2023-08-08 14:47:13 -0500

    update layout.js, page.js and src/components/svgs/index.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,32 @@
 import Navbar from "@/components/Navbar/navbar";
-
 import Hero from "@/components/Hero/Hero";
+import MobileCardLink from "@/components/MobileCardLink/MobileCardLink";
 
 export default function Home() {
+  const mobileCardLinks = [
+    ["WEB DESIGN", "/assets/home/mobile/image-web-design.jpg", "Web Design"],
+    ["APP DESIGN", "/assets/home/mobile/image-app-design.jpg", "App Design"],
+    [
+      "GRAPHIC DESIGN",
+      "/assets/home/mobile/image-graphic-design.jpg",
+      "Graphic Design",
+    ],
+  ];
   return (
     <>
       <Navbar /> <Hero />
+      <div className="w-[375px] mt-[120px]">
+        <div className="flex flex-col gap-[24px] items-center">
+          {mobileCardLinks.map((image) => (
+            <MobileCardLink
+              alt={image[2]}
+              name={image[0]}
+              imageUrl={image[1]}
+            />
+          ))}
+          <div></div>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/svgs/index.js
+++ src/components/svgs/index.js
@@ -1,3 +1,4 @@
 export { default as IconHamburger } from "../../../public/assets/shared/mobile/icon-hamburger.svg";
 export { default as BackgroundPatternSmallCircle } from "../../../public/assets/shared/desktop/bg-pattern-small-circle.svg";
 export { default as BackgroundPatternHeroHome } from "../../../public/assets/home/desktop/bg-pattern-hero-home.svg";
+export { default as IconRightArrow } from "../../../public/assets/shared/desktop/icon-right-arrow.svg";
