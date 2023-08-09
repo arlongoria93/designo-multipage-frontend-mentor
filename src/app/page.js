caf547cdd5728e0479d5428b04cab99bf1954686
Angelo <arlongoria93@gmail.com>
2023-08-08 20:56:39 -0500

    update page.js and src/components/svgs/index.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,13 @@
 import Navbar from "@/components/Navbar/navbar";
 import Hero from "@/components/Hero/Hero";
 import MobileCardLink from "@/components/MobileCardLink/MobileCardLink";
+import {
+  BackgroundPatternHeroHome,
+  BackgroundPatternSmallCircle,
+  IllustrationFriendly,
+  IllustrationPassionate,
+  IllustrationResourceful,
+} from "@/components/svgs";
 
 export default function Home() {
   const mobileCardLinks = [
@@ -24,7 +31,51 @@ export default function Home() {
               imageUrl={image[1]}
             />
           ))}
-          <div></div>
+          <div className="flex flex-col mt-[96px] items-center justify-center gap-[80px]">
+            <div className="flex flex-col w-[327px] justify-center  items-center">
+              <div className="relative w-[202px] h-[202px]">
+                <BackgroundPatternSmallCircle className="-z-10 w-[202px] h-[202px]  absolute object-center" />
+                <IllustrationPassionate className="w-full h-full z-10" />
+              </div>
+              <h1 className="text-[20px] font-medium leading-[26px] tracking-[5px] text-[#333136] mb-[32px]">
+                PASSIONATE
+              </h1>
+              <p className="text-[16px] leading-[26px] text-center">
+                Each project starts with an in-depth brand research to ensure we
+                only create products that serve a purpose. We merge art, design,
+                and technology into exciting new solutions.
+              </p>
+            </div>
+            <div className="flex flex-col w-[327px] justify-center  items-center">
+              <div className="relative w-[202px] h-[202px]">
+                <BackgroundPatternSmallCircle className="-z-10 w-[202px] h-[202px]  absolute object-center" />
+                <IllustrationResourceful className="w-full h-full z-10" />
+              </div>
+              <h1 className="text-[20px] font-medium leading-[26px] tracking-[5px] text-[#333136] mb-[32px]">
+                RESOURCEFUL
+              </h1>
+              <p className="text-[16px] leading-[26px] text-center">
+                Everything that we do has a strategic purpose. We use an agile
+                approach in all of our projects and value customer
+                collaboration. It guarantees superior results that fulfill our
+                clients’ needs.
+              </p>
+            </div>
+            <div className="flex flex-col w-[327px] justify-center  items-center">
+              <div className="relative w-[202px] h-[202px]">
+                <BackgroundPatternSmallCircle className="-z-10 w-[202px] h-[202px]  absolute object-center" />
+                <IllustrationFriendly className="w-full h-full z-10" />
+              </div>
+              <h1 className="text-[20px] font-medium leading-[26px] tracking-[5px] text-[#333136] mb-[32px]">
+                FRIENDLY
+              </h1>
+              <p className="text-[16px] leading-[26px] text-center">
+                We are a group of enthusiastic folks who know how to put people
+                first. Our success depends on our customers, and we strive to
+                give them the best experience a company can provide.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/components/svgs/index.js
+++ src/components/svgs/index.js
@@ -2,3 +2,6 @@ export { default as IconHamburger } from "../../../public/assets/shared/mobile/i
 export { default as BackgroundPatternSmallCircle } from "../../../public/assets/shared/desktop/bg-pattern-small-circle.svg";
 export { default as BackgroundPatternHeroHome } from "../../../public/assets/home/desktop/bg-pattern-hero-home.svg";
 export { default as IconRightArrow } from "../../../public/assets/shared/desktop/icon-right-arrow.svg";
+export { default as IllustrationPassionate } from "../../../public/assets/home/desktop/illustration-passionate.svg";
+export { default as IllustrationResourceful } from "../../../public/assets/home/desktop/illustration-resourceful.svg";
+export { default as IllustrationFriendly } from "../../../public/assets/home/desktop/illustration-friendly.svg";
