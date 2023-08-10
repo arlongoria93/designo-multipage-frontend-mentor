dd0f5f6643648d10a6b9f79b0a957c6fe844b46f
Angelo <arlongoria93@gmail.com>
2023-08-10 14:47:00 -0500

    update HomeContent.js with bottom orange card

---
+++ src/components/DesktopCardLink/DesktopCardLink.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { IconRightArrow } from "@/components/svgs";
+
+const DesktopCardLink = ({ name, imageUrl, alt }) => {
+  return (
+    <div className="bg-[#000] w-[689px] lg:w-[541px] flex flex-col items-center rounded-[15px] h-[250px] lg:h-[308px] relative justify-center">
+      <Image
+        src={imageUrl}
+        layout="fill"
+        alt={alt}
+        quality={100}
+        className="opacity-[0.49] rounded-[15px] "
+      />
+      <Link
+        className="text-white font-medium   flex flex-col z-20"
+        href="/mobile/web-design"
+      >
+        <h1 className="leading-[48px] mb-[11.97px] tracking-[2px] text-[40px]">
+          {name}
+        </h1>
+        <div className="flex flex-row items-center justify-center gap-[16px] ">
+          <h2 className="text-[15px] tracking-[5px] uppercase ">
+            View Projects
+          </h2>
+
+          <IconRightArrow className="w-[7px] h-[10px] " />
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default DesktopCardLink;
--- src/components/HomeContent/HomeContent.js
+++ src/components/HomeContent/HomeContent.js
@@ -2,6 +2,9 @@ import React from "react";
 import Image from "next/image";
 import MobileCardLink from "@/components/MobileCardLink/MobileCardLink";
 import TabletCardLink from "../TabletCardLink/TabletCardLink";
+import DesktopCardLink from "../DesktopCardLink/DesktopCardLink";
+import Link from "next/link";
+import { IconRightArrow } from "@/components/svgs";
 import {
   BackgroundPatternSmallCircle,
   IllustrationFriendly,
@@ -23,12 +26,26 @@ const mobileCardLinks = [
     "Graphic Design",
   ],
 ];
-const TabletCardLinks = [
+const tabletCardLinks = [
   ["WEB DESIGN", "/assets/home/tablet/image-web-design.jpg", "Web Design"],
   ["APP DESIGN", "/assets/home/tablet/image-app-design.jpg", "App Design"],
   [
     "GRAPHIC DESIGN",
     "/assets/home/tablet/image-graphic-design.jpg",
+    "Graphic Design",
+  ],
+];
+
+const desktopCardLinks = [
+  [
+    "WEB DESIGN",
+    "/assets/home/desktop/image-web-design-large.jpg",
+    "Web Design",
+  ],
+  ["APP DESIGN", "/assets/home/desktop/image-app-design.jpg", "App Design"],
+  [
+    "GRAPHIC DESIGN",
+    "/assets/home/desktop/image-graphic-design.jpg",
     "Graphic Design",
   ],
 ];
@@ -46,8 +63,8 @@ const HomeContent = () => {
             />
           ))}
         </div>
-        <div className="hidden md:flex flex-col gap-[24px] items-center">
-          {TabletCardLinks.map((image) => (
+        <div className="hidden lg:hidden md:flex flex-col gap-[24px] items-center">
+          {tabletCardLinks.map((image) => (
             <TabletCardLink
               alt={image[2]}
               name={image[0]}
@@ -55,33 +72,73 @@ const HomeContent = () => {
             />
           ))}
         </div>
-        <div className="flex flex-col mt-[96px] items-center justify-center gap-[80px]">
-          <div className="flex flex-col md:flex-row  justify-center  items-center md:gap-[48px]">
+        <div className="hidden lg:flex lg:flex-row gap-[30px]">
+          <div className="bg-[#000] w-[541px] h-[640px] flex flex-col items-center rounded-[15px]  relative justify-center">
+            <Image
+              src={desktopCardLinks[0][1]}
+              layout="fill"
+              alt={desktopCardLinks[0][2]}
+              quality={100}
+              className="opacity-[0.49] rounded-[15px] "
+            />
+            <Link
+              className="text-white font-medium   flex flex-col z-20"
+              href="/mobile/web-design"
+            >
+              <h1 className="leading-[48px] mb-[11.97px] tracking-[2px] text-[40px]">
+                {desktopCardLinks[0][0]}
+              </h1>
+              <div className="flex flex-row items-center justify-center gap-[16px] ">
+                <h2 className="text-[15px] tracking-[5px] uppercase ">
+                  View Projects
+                </h2>
+
+                <IconRightArrow className="w-[7px] h-[10px] " />
+              </div>
+            </Link>
+          </div>
+          <div className="flex flex-col gap-[24px] justify-between">
+            <DesktopCardLink
+              imageUrl={desktopCardLinks[1][1]}
+              alt={desktopCardLinks[1][2]}
+              name={desktopCardLinks[1][0]}
+            />
+            <DesktopCardLink
+              imageUrl={desktopCardLinks[2][1]}
+              alt={desktopCardLinks[2][2]}
+              name={desktopCardLinks[2][0]}
+            />
+          </div>
+        </div>
+
+        {/* PASSIONATE _ RESOURCEFUL _ FRIENDLY BLOCKS */}
+        <div className="flex flex-col lg:flex-row lg:w-[1111px] mt-[96px] lg:mt-[160px] items-center justify-center gap-[80px] lg:gap-[30px] overflow-hidden">
+          <div className="flex flex-col  md:flex-row lg:flex-col  justify-center  items-center md:gap-[48px] lg:gap-[32px]">
             <div className="relative w-[202px] h-[202px]">
               <BackgroundPatternSmallCircle className="-z-10 w-[202px] h-[202px]  absolute object-center" />
               <IllustrationPassionate className="w-full h-full z-10" />
             </div>
-            <div className="flex flex-col items-center md:items-start md:text-start w-[327px] md:w-[439px]">
+            <div className="flex flex-col items-center md:items-start md:text-start w-[327px] md:w-[439px] lg:w-[350px] lg:items-center lg:text-center">
               <h1 className="text-[20px] font-medium leading-[26px] tracking-[5px] text-[#333136] mb-[32px]">
                 PASSIONATE
               </h1>
-              <p className="text-[16px] leading-[26px] text-center md:text-start">
+              <p className="text-[16px] leading-[26px] text-center md:text-start lg:items-center lg:text-center">
                 Each project starts with an in-depth brand research to ensure we
                 only create products that serve a purpose. We merge art, design,
                 and technology into exciting new solutions.
               </p>
             </div>
           </div>
-          <div className="flex flex-col md:flex-row  justify-center  items-center md:gap-[48px]">
+          <div className="flex flex-col md:flex-row lg:flex-col  justify-center  items-center md:gap-[48px] lg:gap-[32px]">
             <div className="relative w-[202px] h-[202px]">
               <BackgroundPatternSmallCircle className="-z-10 w-[202px] h-[202px]  absolute object-center" />
               <IllustrationResourceful className="w-full h-full z-10" />
             </div>
-            <div className="flex flex-col items-center md:items-start md:text-start w-[327px] md:w-[439px]">
-              <h1 className="text-[20px] font-medium leading-[26px] tracking-[5px] text-[#333136] mb-[32px]">
+            <div className="flex flex-col items-center md:items-start md:text-start w-[327px] md:w-[439px] lg:w-[350px] lg:items-center lg:text-center">
+              <h1 className="text-[20px] font-medium leading-[26px] tracking-[5px] text-[#333136] mb-[32px] ">
                 RESOURCEFUL
               </h1>
-              <p className="text-[16px] leading-[26px] text-center md:text-start">
+              <p className="text-[16px] leading-[26px] text-center md:text-start lg:items-center lg:text-center">
                 Everything that we do has a strategic purpose. We use an agile
                 approach in all of our projects and value customer
                 collaboration. It guarantees superior results that fulfill our
@@ -89,16 +146,16 @@ const HomeContent = () => {
               </p>
             </div>
           </div>
-          <div className="flex flex-col md:flex-row  justify-center  items-center md:gap-[48px]">
+          <div className="flex flex-col md:flex-row lg:flex-col justify-center  items-center md:gap-[48px] lg:gap-[32px]">
             <div className="relative w-[202px] h-[202px]">
               <BackgroundPatternSmallCircle className="-z-10 w-[202px] h-[202px]  absolute object-center" />
               <IllustrationFriendly className="w-full h-full z-10" />
             </div>
-            <div className="flex flex-col items-center md:items-start md:text-start w-[327px] md:w-[439px]">
-              <h1 className="text-[20px] font-medium leading-[26px] tracking-[5px] text-[#333136] mb-[32px]">
+            <div className="flex flex-col items-center md:items-start md:text-start lg:items-center lg:text-center w-[327px] md:w-[439px] lg:w-[350px]">
+              <h1 className="text-[20px] font-medium leading-[26px] tracking-[5px] text-[#333136] mb-[32px] lg:h-[26px]">
                 FRIENDLY
               </h1>
-              <p className="text-[16px] leading-[26px] text-center md:text-start">
+              <p className="text-[16px] leading-[26px] text-center md:text-start lg:items-center lg:text-center lg:h-[104px] lg:w-[304px]">
                 We are a group of enthusiastic folks who know how to put people
                 first. Our success depends on our customers, and we strive to
                 give them the best experience a company can provide.
@@ -106,14 +163,15 @@ const HomeContent = () => {
             </div>
           </div>
         </div>
+        {/* Fooooooot3r */}
         <div className=" mt-[311px] flex flex-col items-center w-full bg-[#1D1C1E] h-[815px] md:h-[423px]">
-          <div className="mt-[-189px] md:mt-[-280px] gap-[49px]  bg-[#E7816B] h-[379px] md:h-[350px] w-[327px] md:w-[690px] items-center justify-center rounded-[15px] flex flex-col overflow-hidden absolute">
-            <BackgroundPatternDesignPagesIntroMobile className="w-[876px] -translate-x-[260px] h-[584px]   absolute" />
-            <div className="flex flex-col text-center items-center justify-center  gap-[6px]">
-              <h1 className="text-[32px] w-[279px] md:text-[40px] font-medium leading-9 md:leading-10 text-white">
+          <div className="mt-[-189px] md:mt-[-280px] gap-[49px]  bg-[#E7816B] h-[379px] md:h-[350px] w-[327px] md:w-[690px] lg:w-[1111px] items-center justify-center rounded-[15px] flex flex-col lg:flex-row lg:justify-around overflow-hidden absolute">
+            <BackgroundPatternDesignPagesIntroMobile className="w-[876px] -translate-x-[260px] h-[584px]   absolute z-0" />
+            <div className="flex flex-col text-center lg:text-start items-center lg:items-start justify-center  gap-[6px] z-20">
+              <h1 className="text-[32px] w-[279px] md:text-[40px] font-medium leading-9 md:leading-10 text-white lg:w-[268px] lg:h-[98px] lg:leading-10">
                 Let's talk about your project
               </h1>
-              <p className="text-white leading-[25px] text-[15px] md:text-[16px] md:leading-[26px] w-[279px] md:w-[573px]">
+              <p className="text-white leading-[25px] text-[15px] md:text-[16px] md:leading-[26px] w-[279px] md:w-[573px] lg:w-[459px] lg:h-[78px]">
                 Ready to take it to the next level? Contact us today and find
                 out how our expertise can help your business grow.
               </p>
