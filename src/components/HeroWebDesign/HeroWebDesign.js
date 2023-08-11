0db017655b52ad31ce57a141bc35095bbee2e914
Angelo <arlongoria93@gmail.com>
2023-08-11 11:19:56 -0500

    hero changes done

--- src/components/HeroWebDesign/HeroWebDesign.js
+++ src/components/HeroWebDesign/HeroWebDesign.js
@@ -16,7 +16,7 @@ const HeroWebDesign = () => {
       className={`${jost.className} md:w-[689px] lg:w-[1111px] mx-auto   md:rounded-[15px] h-[320px] md:h-[252px]  bg-[#E7816B] flex flex-col lg:flex-row z-10 overflow-clip items-center`}
     >
       <div className="relative">
-        <BackgroundPatternIntroWeb className=" translate-x-[-78.5%] md:translate-x-[-43%] md:translate-y-[-29%]  object-contain  h-[584px] w-[876px]  absolute overflow-hidden " />
+        <BackgroundPatternIntroWeb className=" translate-x-[-78.5%] md:translate-x-[-43%] lg:translate-x-[26.5%] md:translate-y-[-29%] lg:translate-y-[-50%]  object-contain  h-[584px] w-[876px]  absolute overflow-hidden " />
       </div>
       <div className="flex flex-col  gap-[24px] mt-[105px] w-[327px] h-[110px] md:w-[573px] lg:w-full lg:justify-center lg:h-[350px]  z-20 md:items-center md:mt-[64px] lg:mt-0 ">
         <h1 className="text-white text-center font-medium  text-[32px] md:text-[48px] lg:w-full leading-[36px] md:leading-[48px]">
