5758565d358f95dcefad178cefeb3f4ab9377615
Angelo <arlongoria93@gmail.com>
2023-08-16 12:31:08 -0500

    removed styling for dark theme

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -37,7 +37,7 @@ const About = () => {
         className={`${jost.className} w-full  lg:w-[1111px] mx-auto   md:rounded-b-[15px] h-[413px] md:h-[352px]  bg-[#E7816B] flex flex-col lg:flex-row z-10 overflow-clip items-center  lg:hidden`}
       >
         <div className="relative">
-          <BackgroundPatternIntroAbout className=" translate-x-[-78.5%] translate-y-[-20%] md:translate-x-[-43%] lg:translate-x-[26.5%] md:translate-y-[-29%] lg:translate-y-[-50%]  object-contain items-center  justify-center h-[584px] w-[876px]  absolute overflow-hidden " />
+          <BackgroundPatternIntroAbout className=" translate-x-[-78.5%]  md:translate-x-[-43%] lg:translate-x-[26.5%]   object-contain items-center  justify-center h-[584px] w-[876px]  absolute overflow-hidden " />
         </div>
         <div className="flex flex-col  gap-[24px] h-full items-center justify-center  w-[327px]  md:w-[573px] lg:w-full lg:justify-center lg:h-[350px]  z-20 md:items-center  lg:mt-0 ">
           <h1 className="text-white text-center font-medium  text-[32px] md:text-[48px] lg:w-full leading-[36px] md:leading-[48px]">
@@ -52,7 +52,26 @@ const About = () => {
           </p>
         </div>
       </main>
-
+      <div className="flex flex-col  bg-gradient-to-t  from-[#FDF3F0] to-[#5D020200] h-[584px] items-center mt-[80px] justify-center">
+        <div className="flex flex-col w-[327px] gap-[24px] text-center">
+          <h1 className="text-[32px] leading-[36px] font-medium text-center text-[#E7816B]">
+            World-class talent
+          </h1>
+          <p className="">
+            We are a crew of strategists, problem-solvers, and technologists.
+            Every design is thoughtfully crafted from concept to launch,
+            ensuring success in its given market. We are constantly updating our
+            skills in a myriad of platforms.
+          </p>
+          <p>
+            Our team is multi-disciplinary and we are not merely interested in
+            form — content and meaning are just as important. We give great
+            importance to craftsmanship, service, and prompt delivery. Clients
+            have always been impressed with our high-quality outcomes that
+            encapsulates their brand’s story and mission.
+          </p>
+        </div>
+      </div>
       <Footer />
     </div>
   );
