11e188a715d1da7219f8ab5557dd68aa206b1b3f
Angelo <arlongoria93@gmail.com>
2023-08-21 11:58:14 -0500

    update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import {
@@ -9,27 +10,32 @@ import {
   IconYoutube,
 } from "@/components/svgs";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const Footer = () => {
+  const router = useRouter();
   return (
-    <div className=" mt-[311px] md:mt-[411px] flex flex-col items-center w-full bg-[#1D1C1E] h-[815px] md:h-[423px]">
-      <div className="mt-[-189px] md:mt-[-280px] lg:mt-[-230px] gap-[49px]  bg-[#E7816B] h-[379px] md:h-[350px] lg:h-[292px] w-[327px] md:w-[690px] lg:w-[1111px] items-center justify-center rounded-[15px] flex flex-col lg:flex-row lg:justify-around overflow-hidden absolute">
-        <BackgroundPatternDesignPagesIntroMobile className="w-[876px] -translate-x-[260px] h-[584px]   absolute z-0" />
-        <div className="flex flex-col text-center lg:text-start items-center lg:items-start justify-center  gap-[6px] z-20">
-          <h1 className="text-[32px] w-[279px] md:text-[40px] font-medium leading-9 md:leading-10 text-white lg:w-[268px] lg:h-[98px] lg:leading-10">
+    <div className=" mt-[311px] flex h-[815px] w-full flex-col items-center bg-[#1D1C1E] md:mt-[411px] md:h-[423px]">
+      <div className="absolute mt-[-189px] flex h-[379px]  w-[327px] flex-col items-center justify-center gap-[49px] overflow-hidden rounded-[15px] bg-[#E7816B] md:mt-[-280px] md:h-[350px] md:w-[690px] lg:mt-[-230px] lg:h-[292px] lg:w-[1111px] lg:flex-row lg:justify-around">
+        <BackgroundPatternDesignPagesIntroMobile className="absolute z-0 h-[584px]   w-[876px] -translate-x-[260px]" />
+        <div className="z-20 flex flex-col items-center justify-center gap-[6px] text-center  lg:items-start lg:text-start">
+          <h1 className="w-[279px] text-[32px] font-medium leading-9 text-white md:text-[40px] md:leading-10 lg:h-[98px] lg:w-[268px] lg:leading-10">
             Let's talk about your project
           </h1>
-          <p className="text-white leading-[25px] text-[15px] md:text-[16px] md:leading-[26px] w-[279px] md:w-[573px] lg:w-[459px] lg:h-[78px]">
+          <p className="w-[279px] text-[15px] leading-[25px] text-white md:w-[573px] md:text-[16px] md:leading-[26px] lg:h-[78px] lg:w-[459px]">
             Ready to take it to the next level? Contact us today and find out
             how our expertise can help your business grow.
           </p>
         </div>
-        <button className="w-[156px] hover:cursor-pointer  h-[56px] rounded-[8px] bg-white text-[15px] font-medium tracking-[1px] lg:hover:bg-[#FFAD9B] lg:hover:text-white z-10">
+        <button
+          className="z-10 h-[56px]  w-[156px] rounded-[8px] bg-white text-[15px] font-medium tracking-[1px] hover:cursor-pointer lg:hover:bg-[#FFAD9B] lg:hover:text-white"
+          onClick={() => router.push("/contact")}
+        >
           GET IN TOUCH
         </button>
       </div>
-      <div className="mt-[253px] md:mt-[150px] md:w-[690px] lg:w-[1111px] flex flex-col w-full items-center gap-[32px]">
-        <div className="flex flex-col md:w-full items-center   md:justify-between gap-[32px] md:flex-row">
+      <div className="mt-[253px] flex w-full flex-col items-center gap-[32px] md:mt-[150px] md:w-[690px] lg:w-[1111px]">
+        <div className="flex flex-col items-center gap-[32px]   md:w-full md:flex-row md:justify-between">
           <Link href="/">
             <Image
               src="/assets/shared/desktop/logo-light.png"
@@ -38,9 +44,9 @@ const Footer = () => {
               alt="logo"
             />
           </Link>
-          <div className="w-[327px] h-[1px] bg-[#fff] md:hidden opacity-10"></div>
+          <div className="h-[1px] w-[327px] bg-[#fff] opacity-10 md:hidden"></div>
 
-          <div className="flex flex-col md:flex-row items-center gap-[32px] text-white leading-[14px] tracking-[2px]">
+          <div className="flex flex-col items-center gap-[32px] leading-[14px] tracking-[2px] text-white md:flex-row">
             <Link href="/company">
               <h1>OUR COMPANY</h1>
             </Link>
@@ -52,25 +58,25 @@ const Footer = () => {
             </Link>
           </div>
         </div>
-        <div className="hidden md:block w-[690px] lg:w-[1111px] h-[1px] bg-[#fff] opacity-10 mt-[40px]"></div>
-        <div className="flex flex-col md:flex-row md:w-[690px] lg:w-[1111px] md:justify-between  gap-[40px] md:gap-[10px]">
-          <div className="flex flex-col items-center md:text-[16px] leading-[26px]  text-white opacity-[0.5]">
+        <div className="mt-[40px] hidden h-[1px] w-[690px] bg-[#fff] opacity-10 md:block lg:w-[1111px]"></div>
+        <div className="flex flex-col gap-[40px] md:w-[690px] md:flex-row md:justify-between  md:gap-[10px] lg:w-[1111px]">
+          <div className="flex flex-col items-center leading-[26px] text-white  opacity-[0.5] md:text-[16px]">
             <h1 className="font-bold">Designo Central Office</h1>
             <h1>3886 Wellington Street</h1>
             <h1>Toronto, Ontario M9C 3J5</h1>
           </div>
-          <div className="flex flex-col md:text-[16px]  items-center md:items-start md:w-[223px] leading-[26px] text-white opacity-[0.5]">
+          <div className="flex flex-col items-center  leading-[26px] text-white opacity-[0.5] md:w-[223px] md:items-start md:text-[16px]">
             <h1 className="font-bold">Contact Us (Central Office)</h1>
             <h1>P : +1 253-863-8967</h1>
             <h1>M : contact@designo.co</h1>
           </div>
 
-          <div className=" md:w-[223px] w-full flex flex-row justify-center items-center md:items-end gap-[24px]">
-            <IconFacebook className="w-[24px] h-[24px] fill-[#E7816B] hover:fill-[#FFAD9B] cursor-pointer" />
-            <IconYoutube className="w-[24px] h-[24px] fill-[#E7816B] hover:fill-[#FFAD9B] cursor-pointer" />
-            <IconTwitter className="w-[24px] h-[24px] fill-[#E7816B] hover:fill-[#FFAD9B] cursor-pointer" />
-            <IconPinterest className="w-[24px] h-[24px] fill-[#E7816B] hover:fill-[#FFAD9B] cursor-pointer" />
-            <IconInstagram className="w-[24px] h-[24px] fill-[#E7816B] hover:fill-[#FFAD9B] cursor-pointer" />
+          <div className=" flex w-full flex-row items-center justify-center gap-[24px] md:w-[223px] md:items-end">
+            <IconFacebook className="h-[24px] w-[24px] cursor-pointer fill-[#E7816B] hover:fill-[#FFAD9B]" />
+            <IconYoutube className="h-[24px] w-[24px] cursor-pointer fill-[#E7816B] hover:fill-[#FFAD9B]" />
+            <IconTwitter className="h-[24px] w-[24px] cursor-pointer fill-[#E7816B] hover:fill-[#FFAD9B]" />
+            <IconPinterest className="h-[24px] w-[24px] cursor-pointer fill-[#E7816B] hover:fill-[#FFAD9B]" />
+            <IconInstagram className="h-[24px] w-[24px] cursor-pointer fill-[#E7816B] hover:fill-[#FFAD9B]" />
           </div>
         </div>
       </div>
